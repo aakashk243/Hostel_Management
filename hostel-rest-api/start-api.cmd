049da2c41f0fdd7daca2706bf4aecd098c4ab999
Karthik <keshavkarthik05@gmail.com>
2026-01-08 23:56:03 +0530

    Revert "Merge branch 'main' of https://github.com/aakashk243/Hostel_Management"
    This reverts commit e25e987000850b9e2972c273f3e484703c619985, reversing
    changes made to 66485d89045e5300bd297ea82ad746289c46531e.

--- hostel-rest-api/start-api.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-SET JAVA_HOME=D:\jdk
-SET RMI_CLASSES=D:\flutter_projects\hostel-rmi-system
-
-"%JAVA_HOME%\bin\java" -cp "target\classes;%RMI_CLASSES%;target\hostel-rest-api-0.0.1-SNAPSHOT.jar" -Dloader.path="%RMI_CLASSES%" -Djava.rmi.server.useCodebaseOnly=false org.springframework.boot.loader.launch.JarLauncher
